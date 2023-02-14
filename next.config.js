1939a2382506adfa60abfdd78af11c998b5792b7
L2HYUNN <donghyuni98@gmail.com>
2023-02-15 00:38:27 +0900

    feat: emotion 패키지 설치 및 관련 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // swcMinify: true, Next.js' swc compiler is used for minification by default since v13.
+  compiler: {
+    emotion: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
