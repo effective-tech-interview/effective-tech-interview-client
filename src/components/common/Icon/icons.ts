88575d3a6cc0de3033279670f939560eba361394
chaaerim <cofla411@ewhain.net>
2023-02-17 02:54:40 +0900

    feat: icon 컴포넌트 구현

---
+++ src/components/common/Icon/icons.ts
@@ -0,0 +1,27 @@
+import { default as check } from './icons/check.svg';
+import { default as downArrow } from './icons/downArrow.svg';
+import { default as hide } from './icons/hide.svg';
+import { default as leftArrow } from './icons/leftArrow.svg';
+import { default as reload } from './icons/reload.svg';
+import { default as rightArrow } from './icons/rightArrow.svg';
+import { default as setting } from './icons/setting.svg';
+import { default as show } from './icons/show.svg';
+import { default as toGoHome } from './icons/toGoHome.svg';
+import { default as upArrow } from './icons/upArrow.svg';
+import { default as user } from './icons/user.svg';
+
+export const ICONS = {
+  user: user,
+  show: show,
+  reload: reload,
+  hide: hide,
+  check: check,
+  downArrow: downArrow,
+  leftArrow: leftArrow,
+  rightArrow: rightArrow,
+  setting: setting,
+  toGoHome: toGoHome,
+  upArrow: upArrow,
+};
+
+export type IconName = keyof typeof ICONS;
---
+++ src/components/common/Icon/index.tsx
@@ -0,0 +1,19 @@
+import type { IconName } from './icons';
+import { ICONS } from './icons';
+
+export interface IconProps extends React.SVGProps<SVGSVGElement> {
+  wrapperProps?: React.HTMLAttributes<HTMLDivElement>;
+  iconName: IconName;
+  size?: number;
+  width?: number;
+  height?: number;
+}
+
+export const Icon = ({ size = 22, width, height, iconName, wrapperProps, ...rest }: IconProps) => {
+  const IconComponent = ICONS[iconName];
+  return (
+    <i {...wrapperProps}>
+      <IconComponent {...rest} width={width || size} height={height || size} />
+    </i>
+  );
+};
