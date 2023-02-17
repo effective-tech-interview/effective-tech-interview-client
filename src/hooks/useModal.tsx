20d42bc1de73723cbc9db3d0601955f61aec4924
chaerim kim <89721027+chaaerim@users.noreply.github.com>
2023-02-17 23:50:07 +0900

    feat: modal 컴포넌트 구현 (#4)
    * feat: modal 컴포넌트 생성
    
    * feat: modal 컴포넌트 구현
    
    * refactor: type 변경 및 리뷰 반영
    
    * refactor: modal interface 제거

---
+++ src/hooks/useModal.tsx
@@ -0,0 +1,48 @@
+import { useRef } from 'react';
+import styled from '@emotion/styled';
+import { useOverlay } from '@toss/use-overlay';
+import type { ComponentProps } from 'react';
+
+import { Modal } from '../components/common/Modal';
+
+export function useModal() {
+  const overlay = useOverlay();
+  const ref = useRef<HTMLDivElement>(null);
+
+  const openModal = (props: ComponentProps<typeof Modal>) =>
+    new Promise(resolve => {
+      overlay.open(({ isOpen, close }) => {
+        return (
+          <>
+            {isOpen && (
+              <Dimmer
+                ref={ref}
+                onClick={(e: React.MouseEvent) => {
+                  {
+                    e.target === ref.current ? close() : null;
+                  }
+                  resolve(false);
+                }}
+              >
+                <Modal {...props} />
+              </Dimmer>
+            )}
+          </>
+        );
+      });
+    });
+
+  return { openModal, close: overlay.close };
+}
+
+export default useModal;
+
+const Dimmer = styled.div`
+  display: flex;
+  align-items: center;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0%;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
