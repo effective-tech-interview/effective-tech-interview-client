b10d85cc3f7bfdec2dad862691bb9cc80fd02b0a
L2HYUNN <donghyuni98@gmail.com>
2023-02-21 02:23:48 +0900

    feat: mount, update 관련 hooks 추가

---
+++ src/hooks/useDidUpdate.ts
@@ -0,0 +1,36 @@
+import { useEffect, useMemo, useRef } from 'react';
+
+import useDidMount from './useDidMount';
+import useWillUnmount from './useWillUnmount';
+
+function useDidUpdate(callback: () => void, conditions?: unknown[]): void {
+  const hasMountedRef = useRef<boolean>(false);
+  const internalConditions = useMemo(() => {
+    if (typeof conditions !== 'undefined' && !Array.isArray(conditions)) {
+      return [conditions];
+    } else if (Array.isArray(conditions) && conditions.length === 0) {
+      console.warn(
+        'Using [] as the second argument makes useDidUpdate a noop. The second argument should either be `undefined` or an array of length greater than 0.'
+      );
+    }
+
+    return conditions;
+  }, [conditions]);
+
+  useEffect(() => {
+    if (hasMountedRef.current) {
+      callback();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, internalConditions);
+
+  useDidMount(() => {
+    hasMountedRef.current = true;
+  });
+
+  useWillUnmount(() => {
+    hasMountedRef.current = false;
+  });
+}
+
+export { useDidUpdate };
