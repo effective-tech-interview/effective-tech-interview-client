4d35e3012f2626508c9575417494cdb1a28ba3db
L2HYUNN <donghyuni98@gmail.com>
2023-03-04 01:04:21 +0900

    fix: jest 설정 변경

--- jest.config.js
+++ jest.config.js
@@ -9,12 +9,9 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '^~/(.*)$': '<rootDir>/src/$1',
-    '^~/components/(.*)$': '<rootDir>/src/components/$1',
-    '^~/pages/(.*)$': '<rootDir>/src/pages/$1',
-    '^~/styles/(.*)$': '<rootDir>/src/styles/$1',
-    '^~/public/(.*)$': '<rootDir>/public/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['<rootDir>/src/__tests__/utils/*'],
 };
 
 module.exports = createJestConfig(customJestConfig);
