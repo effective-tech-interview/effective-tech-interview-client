9948cd86b991967eb3418ae01763a5537d4b86bf
L2HYUNN <donghyuni98@gmail.com>
2023-03-11 00:41:06 +0900

    feat: category 페이지 추가

---
+++ src/pages/category/index.tsx
@@ -0,0 +1,50 @@
+import { useRouter } from 'next/router';
+import { dehydrate, QueryClient } from '@tanstack/react-query';
+import { Flex, Spacing } from '@toss/emotion-utils';
+import { useRecoilValue } from 'recoil';
+
+import { getMainCategories } from '~/apis';
+import { SingleBottomFixedButton } from '~/components/common/Button';
+import Select from '~/components/common/Select';
+import Text from '~/components/common/Text';
+import { MAIN_CATEGORY_QUERY_KEYS } from '~/constants/queryKeys';
+import { useMainCategoryQuery } from '~/hooks/query/useMainCategoryQuery';
+import selectedCategoryIdAtomFamily from '~/store/selectedCategoryId/selectedCategoryIdAtom';
+
+export async function getStaticProps() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(MAIN_CATEGORY_QUERY_KEYS.getMainCategories, getMainCategories);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
+
+export default function Category() {
+  const { data: mainCategoryData } = useMainCategoryQuery();
+
+  const selectedCategoryId = useRecoilValue(selectedCategoryIdAtomFamily('main'));
+
+  const router = useRouter();
+
+  return (
+    <>
+      <Spacing size={80} />
+      <Flex.Center direction="column">
+        <Text variant="h2">희망하는 직무를 선택해주세요</Text>
+        <Spacing size={40} />
+        <Select items={mainCategoryData?.categories} />
+        <SingleBottomFixedButton
+          variant="largePrimary"
+          disabled={selectedCategoryId === 0}
+          onClick={() => router.push(`/category/${selectedCategoryId}`)}
+        >
+          완료
+        </SingleBottomFixedButton>
+      </Flex.Center>
+    </>
+  );
+}
