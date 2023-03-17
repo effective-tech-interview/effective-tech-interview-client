22c6622df03076b422f4cf80b51ed7a45d3383f5
L2HYUNN <donghyuni98@gmail.com>
2023-03-17 19:17:28 +0900

    feat: jest svg 관련 설정 수정 및 추가

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,22 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
   dir: './',
 });
 
-const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleNameMapper: {
-    '^~/(.*)$': '<rootDir>/src/$1',
-  },
-  testEnvironment: 'jest-environment-jsdom',
-  testPathIgnorePatterns: ['<rootDir>/src/__tests__/utils/*'],
+const jestConfig = async () => {
+  const nextJestConfig = await createJestConfig()();
+  return {
+    ...nextJestConfig,
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    testEnvironment: 'jest-environment-jsdom',
+    moduleNameMapper: {
+      '^~/(.*)$': '<rootDir>/src/$1',
+      '\\.svg$': '<rootDir>/__mocks__/svg.js',
+    },
+    testPathIgnorePatterns: ['<rootDir>/src/__tests__/utils/*'],
+  };
 };
 
-module.exports = createJestConfig(customJestConfig);
+module.exports = jestConfig;
