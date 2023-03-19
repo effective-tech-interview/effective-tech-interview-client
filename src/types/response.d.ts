e46b32cf969d2783bff3780d563e509e9570c974
DongHyun Lee <donghyuni98@gmail.com>
2023-03-19 13:34:21 +0900

    feat: question answer 기능 구현 (#31)
    * feat: main 카테고리 호출 api 로직 추가
    
    * feat: mid category api 관련 로직 추가
    
    * feat: question 관련 api 로직 추가 및 일부 네이밍 수정
    
    * feat: question 페이지 추가
    
    * feat: question answer 관련 api 로직 추가
    
    * feat: double bottom fixed button 추가
    
    * feat: question answer 로직 추가
    
    * chore: 불필요한 변경 제거

--- src/types/response.d.ts
+++ src/types/response.d.ts
@@ -2,19 +2,28 @@ interface MainCategoryResponse {
   id: number;
   name: string;
 }
+
 interface MainCategoriesResponse {
   categories: MainCategoryResponse[];
 }
+
 interface MidCategoryResponse {
   id: number;
   imageUrl: string;
   name: string;
 }
+
 interface MidCategoriesResponse {
   name: string;
   categories: MidCategoryResponse[];
 }
+
 interface RandomQuestionResponse {
   id: number;
   question: string;
+}
+
+interface QuestionAnswerResponse {
+  id: number;
+  answer: string;
 }
