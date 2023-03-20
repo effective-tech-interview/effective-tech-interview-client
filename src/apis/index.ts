e3ffc77d34be93f5c9338863971853d2b01027dd
L2HYUNN <donghyuni98@gmail.com>
2023-03-20 18:53:47 +0900

    refactor: api base url 수정 및 deprecated api 명시

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -7,51 +7,51 @@ export type LoginResponse = {
 };
 
 export async function postEmail(email: string) {
-  return await axiosClient.post('/auth/email/send', { email });
+  return await axiosClient.post('/v1/auth/email/send', { email });
 }
 
 export async function postEmailAndCode(email: string, verificationCode: number) {
-  return await axiosClient.post('/auth/email/authenticate', { email, verificationCode });
+  return await axiosClient.post('/v1/auth/email/authenticate', { email, verificationCode });
 }
 
 export async function postSignUp(email: string, password: string, confirmPassword: string) {
-  return await axiosClient.post('/signup', { email, password, confirmPassword });
+  return await axiosClient.post('/v1/signup', { email, password, confirmPassword });
 }
 
 export async function postLogin(email: string, password: string) {
   const {
     data: { memberId, accessToken, refreshToken },
-  } = await axiosClient.post<LoginResponse>('/auth/login', { email, password });
+  } = await axiosClient.post<LoginResponse>('/v1/auth/login', { email, password });
   return { memberId, accessToken, refreshToken };
 }
 
 //password reset apis
 export async function postEmailforResetPassword(email: string) {
-  return await axiosClient.post('/auth/password-reset/email', { email });
+  return await axiosClient.post('/v1/auth/password-reset/email', { email });
 }
 
 export async function postEmailAndCodeforResetPassword(email: string, verificationCode: number) {
-  return await axiosClient.post('/auth/password-reset/email-verification', {
+  return await axiosClient.post('/v1/auth/password-reset/email-verification', {
     email,
     verificationCode,
   });
 }
 
 export async function postResetPassword(email: string, password: string, confirmPassword: string) {
-  return await axiosClient.post('/members/password-reset', { email, password, confirmPassword });
+  return await axiosClient.post('/v1/members/password-reset', { email, password, confirmPassword });
 }
 
 export const getMainCategories = async () => {
   const {
     data: { categories },
-  } = await axiosClient.get<MainCategoriesResponse>('/categories/main');
+  } = await axiosClient.get<MainCategoriesResponse>('/v1/categories/main');
   return { categories };
 };
 
 export const getMidCategories = async (mainCategoryId: number) => {
   const {
     data: { name, categories },
-  } = await axiosClient.get<MidCategoriesResponse>('/categories/mid', {
+  } = await axiosClient.get<MidCategoriesResponse>('/v1/categories/mid', {
     params: { mainCategoryId },
   });
   return { name, categories };
@@ -60,23 +60,23 @@ export const getMidCategories = async (mainCategoryId: number) => {
 export const getMidCategory = async (midCategoryId: number) => {
   const {
     data: { id, name, imageUrl },
-  } = await axiosClient.get<MidCategoryResponse>(`/categories/mid/${midCategoryId}`);
+  } = await axiosClient.get<MidCategoryResponse>(`/v1/categories/mid/${midCategoryId}`);
   return { id, name, imageUrl };
 };
 
-export const getRandomQuestion = async (midCategoryId: number) => {
+export const getRandomQuestionDeprecated = async (midCategoryId: number) => {
   const {
     data: { id, question },
-  } = await axiosClient.get<RandomQuestionResponse>('/questions', {
+  } = await axiosClient.get<RandomQuestionResponse>('/v1/questions', {
     params: { midCategoryId },
   });
   return { id, question };
 };
 
-export const getQuestionAnswer = async (questionId?: number) => {
+export const getQuestionAnswerDeprecated = async (questionId?: number) => {
   const {
     data: { id, answer },
-  } = await axiosClient.get<QuestionAnswerResponse>(`/questions/${questionId}/answer`, {
+  } = await axiosClient.get<QuestionAnswerResponse>(`/v1/questions/${questionId}/answer`, {
     params: {
       questionId,
     },
