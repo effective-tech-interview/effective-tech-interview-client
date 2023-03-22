cd0b021debdf06235b5f5b51daeed56cec28a22f
chaaerim <cofla411@ewhain.net>
2023-03-22 23:04:35 +0900

    feat: favicon, og-tag 적용

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,19 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link
+        rel="icon"
+        href="https://effective-tech-client-assets.s3.ap-northeast-2.amazonaws.com/logo-blue.svg"
+      />
+      <meta
+        property="og:image"
+        content="https://effective-tech-client-assets.s3.ap-northeast-2.amazonaws.com/og-image.svg"
+      />
+      <meta property="og:title" content="이펙티브 기술면접 - ChatGPT로 연습하는 개발자 면접" />
+      <meta
+        property="og:description"
+        content="프론트엔드, 백엔드 면접 막막하다면? 개발 언어만 선택하면 10초만에 맞춤 질문을 뽑아드려요. 답변이 어려운 꼬리 질문까지, AI로 면접 준비 한번에!"
+      />
       <body>
         <Main />
         <NextScript />
