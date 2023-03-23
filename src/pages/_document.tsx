097e2c0c56f05603227c1ad69d1f693a8fc2cdff
L2HYUNN <donghyuni98@gmail.com>
2023-03-23 22:29:52 +0900

    feat: title ,description seo 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta
+        name="description"
+        content="프론트엔드, 백엔드 면접 막막하다면? 개발 언어만 선택하면 10초만에 맞춤 질문을 뽑아드려요. 답변이 어려운 꼬리 질문까지, AI로 면접 준비 한번에!"
+      />
+
       <link
         rel="icon"
         href="https://effective-tech-client-assets.s3.ap-northeast-2.amazonaws.com/logo-blue.svg"
