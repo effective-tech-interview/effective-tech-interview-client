aa0be7efff130fc66189d55c0b4efe966a066e10
chaerim kim <89721027+chaaerim@users.noreply.github.com>
2023-03-30 00:19:19 +0900

    style: input style 변경 (#57)

--- src/components/common/InputBubble/index.tsx
+++ src/components/common/InputBubble/index.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import type { ChangeEvent } from 'react';
 
@@ -10,33 +8,14 @@ export interface InputProps {
   value?: string;
 }
 export const InputBubble = ({ onChange, value }: InputProps) => {
-  const [isClicked, setIsClicked] = useState(false);
-
-  const onFocus = () => {
-    setIsClicked(true);
-  };
-  const onBlur = () => {
-    setIsClicked(false);
-  };
-
   return (
     <BubbleWrapper>
       <StyledTextArea
         value={value}
         id="input"
-        onFocus={onFocus}
-        onBlur={onBlur}
         onChange={onChange}
         placeholder="답변을 작성해주세요"
       />
-      {isClicked ? (
-        <>
-          <ClickedArrow />
-          <Arrow isClicked={isClicked} />
-        </>
-      ) : (
-        <Arrow isClicked={isClicked} />
-      )}
     </BubbleWrapper>
   );
 };
@@ -58,7 +37,7 @@ const StyledTextArea = styled.textarea`
   color: ${theme.color.gray800};
 
   line-height: 24px;
-  border-radius: 8px 8px 0 8px;
+  border-radius: 12px 12px 0 12px;
   background: ${theme.color.gray100};
   height: 112px;
   resize: none;
@@ -71,34 +50,4 @@ const StyledTextArea = styled.textarea`
     font-size: 16px;
     color: ${theme.color.gray400};
   }
-`;
-
-const Arrow = styled('div')<{ isClicked: boolean }>`
-  position: absolute;
-  bottom: -12px;
-  ${({ isClicked }) => {
-    return css`
-      right: ${isClicked ? '2px' : '0'};
-      bottom: ${isClicked ? '-8px' : '-12px'};
-    `;
-  }}
-  width: 0;
-  height: 0;
-  border-bottom: 7px solid transparent;
-  border-top: 7px solid ${theme.color.gray100};
-  border-left: 6px solid transparent;
-  border-right: 6px solid ${theme.color.gray100};
-  z-index: 1111;
-`;
-
-const ClickedArrow = styled.div`
-  position: absolute;
-  bottom: -12px;
-  right: 0;
-  width: 0;
-  height: 0;
-  border-bottom: 7px solid transparent;
-  border-top: 7px solid ${theme.color.gray200};
-  border-left: 6px solid transparent;
-  border-right: 7px solid ${theme.color.gray200};
 `;
