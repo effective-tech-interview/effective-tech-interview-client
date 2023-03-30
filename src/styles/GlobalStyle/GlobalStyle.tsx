393444291955f58c99f7ca33046ac7878c403a13
chaaerim <cofla411@ewhain.net>
2023-03-30 16:07:20 +0900

    style: layout 수정 및 scrollbar display none

--- src/styles/GlobalStyle/GlobalStyle.tsx
+++ src/styles/GlobalStyle/GlobalStyle.tsx
@@ -14,6 +14,18 @@ const globalCss = css`
     box-sizing: border-box;
   }
 
+  body {
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
+  }
+  body ::-webkit-scrollbar {
+    display: none;
+  }
+
+  div ::-webkit-scrollbar {
+    display: none;
+  }
+
   html {
     font-size: 62.5%;
 
@@ -31,4 +43,6 @@ const globalCss = css`
       color: inherit;
     }
   }
+
+  /* ( 크롬, 사파리, 오페라, 엣지 ) 동작 */
 `;
