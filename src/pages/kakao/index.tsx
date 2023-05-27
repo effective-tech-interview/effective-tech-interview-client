9af785711d07397376149650d9989609dd1a0d0d
chaaerim <cofla411@ewhain.net>
2023-05-27 20:49:33 +0900

    feat: kakao code post api fetching

--- src/pages/kakao/index.tsx
+++ src/pages/kakao/index.tsx
@@ -1,7 +1,37 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useMutation } from '@tanstack/react-query';
+
+import { postKakaoCode } from '~/apis';
+import { isEffError } from '~/apis/client';
+import { clientUrl } from '~/components/homePage/HomeFooter';
 import { useNextRouter } from '~/hooks/useNextRouter';
 
 export default function Kakao() {
   const { query } = useNextRouter({ suspense: true });
-  console.log(query.code);
-  return <h1>hi</h1>;
+
+  const code = query.code;
+  const router = useRouter();
+  const redirectUri = `${clientUrl}kakao`;
+
+  const { mutate: kakaoLoginMuatation } = useMutation(async () => {
+    if (typeof code === 'string') {
+      try {
+        const data = await postKakaoCode(code, redirectUri);
+        const { accessToken } = data;
+        localStorage.setItem('accessToken', accessToken);
+        router.push('/category');
+      } catch (error: unknown) {
+        if (isEffError(error)) {
+          console.log(error);
+        }
+      }
+    }
+  });
+
+  useEffect(() => {
+    kakaoLoginMuatation();
+  }, [kakaoLoginMuatation]);
+
+  return null;
 }
