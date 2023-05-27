c53140a3752be527fa1985ff1c9cf5df39faf227
chaaerim <cofla411@ewhain.net>
2023-05-27 19:48:54 +0900

    feat: add useNextRouter

--- src/pages/kakao/index.tsx
+++ src/pages/kakao/index.tsx
@@ -1,3 +1,7 @@
+import { useNextRouter } from '~/hooks/useNextRouter';
+
 export default function Kakao() {
+  const { query } = useNextRouter({ suspense: true });
+  console.log(query.code);
   return <h1>hi</h1>;
 }
