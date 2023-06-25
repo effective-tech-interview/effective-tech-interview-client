3947db817919950a27ab5ea87fb7e374d1ae10f7
L2HYUNN <donghyuni98@gmail.com>
2023-06-25 14:55:22 +0900

    feat: atomFamily reset 로직 추가

--- src/store/userAnswerReset/userAnswerResetSelector.ts
+++ src/store/userAnswerReset/userAnswerResetSelector.ts
@@ -1,6 +1,9 @@
 import { selector } from 'recoil';
 
+import aiFeedbackButtonDisableAtomFamily from '../aiFeedbackButtonDisable/aiFeedbackButtonDisableAtomFamily';
+import isFeedbackLoadingAtomFamily from '../isFeedbackLoading/isFeedbackLoadingAtomFamily';
 import questionInputDisableAtomFamily from '../questionInputDisable/questionInputDisableAtomFamily';
+import tailQuestionButtonDisableAtomFamily from '../tailQuestionButtonDisable/tailQuestionButtonDisableAtomFamily';
 import userAnswerAtomFamily from '../userAnswer/userAnswerAtomFamily';
 import userAnswerKeyAtom from '../userAnswerKey/userAnswerKeyAtom';
 
@@ -16,6 +19,9 @@ const userAnswerResetSelector = selector({
     userKeys.forEach(key => {
       set(userAnswerAtomFamily(key), '');
       set(questionInputDisableAtomFamily(key), false);
+      set(isFeedbackLoadingAtomFamily(key), false);
+      set(aiFeedbackButtonDisableAtomFamily(key), false);
+      set(tailQuestionButtonDisableAtomFamily(key), false);
     });
   },
 });
