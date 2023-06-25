d31af264a44c50157397e62bee95700451e69d6f
L2HYUNN <donghyuni98@gmail.com>
2023-06-25 14:56:39 +0900

    feat: aiFeedbackBubble 컴포넌트 추가

---
+++ src/components/common/AIFeedbackBubble/index.tsx
@@ -0,0 +1,62 @@
+import styled from '@emotion/styled';
+
+import { theme } from '~/styles/Theme';
+
+import { Chip } from '../Chip';
+import Text from '../Text';
+
+export interface AIBubbleProps {
+  advantage: string;
+  improvements: string;
+}
+
+export const AIFeedbackBubble = ({ advantage, improvements }: AIBubbleProps) => {
+  return (
+    <BubbleWrapper>
+      <BubbleContainer>
+        <ChipWrapper>
+          <Chip chipType="highlight" text="AI 피드백" />
+        </ChipWrapper>
+        <Text variant="b3" color="primary_default">
+          {'좋은 점'}
+        </Text>
+        <Text variant="b3" color="gray800">
+          {advantage}
+        </Text>
+        <Text variant="b3" color="system_default">
+          {'개선할 점'}
+        </Text>
+        <Text variant="b3" color="gray800">
+          {improvements}
+        </Text>
+      </BubbleContainer>
+    </BubbleWrapper>
+  );
+};
+
+export const BubbleWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const BubbleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  padding: 16px;
+  width: 100%;
+  background: ${theme.color.gray000};
+  word-break: break-all;
+  border-radius: 12px 12px 12px 0px;
+`;
+
+export const Divider = styled.div`
+  height: 1px;
+  background: ${theme.color.gray100};
+`;
+
+const ChipWrapper = styled.div`
+  width: max-content;
+`;
